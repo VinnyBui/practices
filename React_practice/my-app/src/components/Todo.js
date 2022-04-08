a2a2ca7105eccf7980d59f21ef57fb788efcc74f
Vinh <vinh.bui0101@gmail.com>
2022-04-08 00:06:33 -0700

    Started react project, added delete modal

---
+++ React_practice/my-app/src/components/Todo.js
@@ -0,0 +1,32 @@
+import {useState} from 'react';
+import Modal from './Modal';
+import Backdrop from './Backdrop';
+
+
+function Todo(props) {
+
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  function deleteHandler(){
+    setModalIsOpen(true);
+
+  }
+
+  function closeModal(){
+    setModalIsOpen(false)
+  }
+
+  return (
+    <div className="card">
+      <h2>{props.text}</h2>
+      <div className="actions">
+        <button className="btn" onClick={deleteHandler}>Delete</button>
+      </div>
+      {modalIsOpen && <Modal/>}
+      {modalIsOpen && <Backdrop onClick={closeModal}/>}
+    </div>
+  );
+}
+
+
+export default Todo;
