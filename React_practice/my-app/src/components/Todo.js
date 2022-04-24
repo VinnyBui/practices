cca1c0b700c0c85d73a797b892f1faa7d269aa3c
Vinh <vinh.bui0101@gmail.com>
2022-04-23 22:13:04 -0700

    Finish Modal practice w/ React

--- React_practice/my-app/src/components/Todo.js
+++ React_practice/my-app/src/components/Todo.js
@@ -5,24 +5,27 @@ import Backdrop from './Backdrop';
 
 function Todo(props) {
 
+  //modalIsOpen is the current state's snapshot ; modalIsOpen = false
+  //setModalIsOpen is the function that allows you to change that value
+  //Whenever you call this state changing function, React will re execute and re evaluate and update what is render on the screen
   const [modalIsOpen, setModalIsOpen] = useState(false);
 
   function deleteHandler(){
     setModalIsOpen(true);
-
   }
 
   function closeModal(){
     setModalIsOpen(false)
   }
 
+  //You can render by doing {modalIsOpen ? <Modal /> : null}
   return (
     <div className="card">
       <h2>{props.text}</h2>
       <div className="actions">
         <button className="btn" onClick={deleteHandler}>Delete</button>
       </div>
-      {modalIsOpen && <Modal/>}
+      {modalIsOpen && <Modal onCancel={closeModal} onConfirm={closeModal} />}
       {modalIsOpen && <Backdrop onClick={closeModal}/>}
     </div>
   );
